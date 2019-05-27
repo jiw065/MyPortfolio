773b5dfc1dea5afd7f8463dca9b4fda0ca18bb7b
jiw065 <wwjjjj0923@gmail.com>
2019-05-26 23:05:46 -0400

    added enlarge image feature
    added enlarge image feature

--- js/main.js
+++ js/main.js
@@ -26,3 +26,10 @@ $(window).scroll(function () {
         $('.totop a').fadeOut();
     }
 });
+
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
